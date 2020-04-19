f78dc1f9949987763463224380cc02b9fa26705c
changzhiwin <changzhiwin@gmail.com>
2020-04-19 20:51:04 +0800

    I want got you

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>我的待办清单</h1>
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import './variables.scss';
+
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
+#app {
+  max-width: 400px;
+  margin: 0 auto;
+  line-height: 1.4;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: $vue-green;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,69 @@
+<template>
+	<div>
+		<BaseInputText 
+			v-model="newTodoText"
+			placeholder="新建待办"
+			@keydown.enter="addTodo"
+		/>
+		<ul v-if="todos.length">
+			<TodoListItem
+				v-for="todo in todos"
+				:key="todo.id"
+				:todo="todo"
+				@remove="removeTodo"
+			/>
+		</ul>
+		<p v-else>
+			Nothing left in the list. Add a new todo in the input above.
+		</p>
+	</div>
+</template>
+
+<script>
+import BaseInputText from './BaseInputText.vue'
+import TodoListItem from './TodoListItem.vue'
+
+let nextTodoId = 1
+
+export default {
+	components: {
+		BaseInputText, TodoListItem
+	},
+  data () {
+    return {
+			newTodoText: '',
+      todos: [
+				{
+					id: nextTodoId++,
+					text: '了解 Vue'
+				},
+				{
+					id: nextTodoId++,
+					text: '学习单文件组件'
+				},
+				{
+					id: nextTodoId++,
+					text: '爱上学习'
+				}
+			]
+    }
+  },
+	methods: {
+		addTodo () {
+			const trimmedText = this.newTodoText.trim()
+			if (trimmedText) {
+				this.todos.push({
+					id: nextTodoId++,
+					text: trimmedText
+				})
+				this.newTodoText = ''
+			}
+		},
+		removeTodo (idToRemove) {
+			this.todos = this.todos.filter(todo => {
+				return todo.id !== idToRemove
+			})
+		}
+	}
+}
+</script>
